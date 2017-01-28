5459340938e362f18675afbd9ec05f6837389d1a
Mark Zepeda <mark@hart.com>
2017-01-28 08:46:02 -0800

    added some error logic. added updating user. added killing user.

--- models/user.js
+++ models/user.js
@@ -5,7 +5,14 @@ let Schema = mongoose.Schema;
 
 let schema = new Schema({
 	name: Schema.Types.String,
-	ble: Schema.Types.String
+	ble: Schema.Types.String,
+	loc: {
+		lat: Schema.Types.Number,
+		long: Schema.Types.Number
+	},
+	isDead: Schema.Types.Boolean,
+	killedBy: Schema.Types.ObjectId,
+	killed: [Schema.Types.ObjectId]
 }, {
   timestamps: true,
 });
