a4e56b005d0fe9d76c35595c1c8c41cdf74ce49f
Jeremy Hummel <jmhummel@gmail.com>
2017-01-27 18:34:51 -0800

    derp

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const koa     = require('koa');
 const app     = koa();
 
 app.use(function *(){
-  this.body = 'Hello, Manhunt!! - garbagecodeabc';
+  this.body = 'Hello, Manhunt!! - garbagecodeabcd';
 });
 
 app.listen(3000);
