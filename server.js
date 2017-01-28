f412e1d2ed62c638b6fe3dc8a184a7d3bf8b037a
Jeremy Hummel <jmhummel@gmail.com>
2017-01-27 17:57:48 -0800

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const koa     = require('koa');
 const app     = koa();
 
 app.use(function *(){
-  this.body = 'Hello, Manhunt!!';
+  this.body = 'Hello, Manhunt!! - test';
 });
 
 app.listen(3000);
