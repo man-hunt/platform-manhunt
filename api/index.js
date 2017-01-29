d02d95847b5ee03ab79cdf14a622c445efcda923
Mark Zepeda <mark@hart.com>
2017-01-28 17:51:41 -0800

    added user state endpoint to see user's nearby enemies. user model changed. {  	name: Schema.Types.String,  	ble: Schema.Types.String,  	loc: { -		lat: Schema.Types.Number, -		long: Schema.Types.Number, -		dir: Schema.Types.Number +		'type': { +			type: Schema.Types.String, +			enum: "Point", +			default: "Point" +		}, +		coordinates: { +			type: [Schema.Types.Number], +			default: [0,0] +		}  	}, +	dir: Schema.Types.Number,  	isDead: Schema.Types.Boolean,  	killedBy: Schema.Types.ObjectId,  	killed: [Schema.Types.ObjectId]  },

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@
 
 const Router    = require('koa-router');
 const User      = require('../models/user');
+const _         = require('lodash');
 
 const router = new Router();
+
+const MAX_HUNT_DISTANCE = 1000; //meters
 
 router.get('/users', function* (){
 	let ctx = this;
@@ -16,6 +19,57 @@ router.get('/users', function* (){
 	}
 	ctx.response.status = 200;
 });
+
+router.get('/users/:userid', function* (){
+	let ctx = this;
+	let user = yield User.findById(ctx.params.userid).exec();
+	if(!user){
+		ctx.response.status = 404;
+		ctx.response.body = {
+			error: "User not found"
+		};
+		return;
+	}
+	let results = yield User.geoNear(user.loc, { maxDistance : MAX_HUNT_DISTANCE, spherical : true })
+	.catch(error => {
+		this.throw(error.message, 400);
+	});
+	let users = _.map(results, function(result){
+		let bearing = calculateDirection(user.loc.coordinates, result.obj.loc.coordinates);
+		return {
+			user: result.obj,
+			metersToUser: result.dis,
+			directionToUser: bearing
+		};
+	});
+	users = _.filter(users, function(test) {
+	  return test.user.id !== user.id;
+	});
+	ctx.response.body = {
+		user: user,
+		nearby: users
+	}
+	ctx.response.status = 200;
+});
+
+const pi4 = Math.PI/4;
+
+const radToDeg = (180 / Math.PI);
+
+// pt = [long, lat] (GeoJSON Point).coordinates
+function calculateDirection(pta, ptb){
+	// Δφ = ln( tan( latB / 2 + π / 4 ) / tan( latA / 2 + π / 4) ) 
+	// Δlon = abs( lonA - lonB ) 
+	// bearing :  θ = atan2( Δlon ,  Δφ ) 
+	pta = [pta[0]/radToDeg, pta[1]/radToDeg];
+	ptb = [ptb[0]/radToDeg, ptb[1]/radToDeg];
+	let dPhi = Math.log( Math.tan(ptb[1]/2 + pi4) /  Math.tan(pta[1]/2 + pi4) );
+	let dLong = Math.abs(pta[0] - ptb[0]);
+	if(dLong > 180)
+		dLong = dLong % 180;
+	let bearing = Math.atan2(dLong, dPhi) * radToDeg;
+	return (bearing + 360) % 360;
+}
 
 router.post('/users', function* (){
 	let ctx = this;
@@ -46,7 +100,9 @@ router.put('/users', function* (){
 		}
 		return;
 	}
-	user.loc = ctx.request.body.loc;
+	user.loc.coordinates.set(0, ctx.request.body.loc.long);
+	user.loc.coordinates.set(1, ctx.request.body.loc.lat);
+	user.dir = ctx.request.body.loc.dir;
 	let savedUser = yield user.save();
 	ctx.response.body = savedUser;
 	ctx.response.status = 200;
--- models/user.js
+++ models/user.js
@@ -7,15 +7,29 @@ let schema = new Schema({
 	name: Schema.Types.String,
 	ble: Schema.Types.String,
 	loc: {
-		lat: Schema.Types.Number,
-		long: Schema.Types.Number,
-		dir: Schema.Types.Number
+		'type': {
+			type: Schema.Types.String, 
+			enum: "Point", 
+			default: "Point"
+		}, 
+		coordinates: { 
+			type: [Schema.Types.Number],   
+			default: [0,0]
+		} 
 	},
+	dir: Schema.Types.Number,
 	isDead: Schema.Types.Boolean,
 	killedBy: Schema.Types.ObjectId,
 	killed: [Schema.Types.ObjectId]
 }, {
   timestamps: true,
+});
+schema.index({loc: '2dsphere'});
+schema.pre('save', function (next) {
+  if (this.isNew && Array.isArray(this.loc) && 0 === this.loc.length) {
+    this.loc = undefined;
+  }
+  next();
 });
 
 let User = mongoose.model('User', schema);
